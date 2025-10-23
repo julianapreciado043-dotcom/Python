4bbdef3d4fd149c256b9ddb31264aefa6543ccb0
julianapreciado043-dotcom <julianapreciado043@gmail.com>
2025-10-23 14:20:37 -0500

    Comentario

--- Practica/Sena/Condicional 3.py
+++ Practica/Sena/Condicional 3.py
@@ -1,8 +1,8 @@
 edad = int(input("Introduce tu edad: "))
 if edad < 4:
-            precio = 0
+        precio = 0
 elif edad <= 18:
-            precio = 4
+        precio = 4
 else:
         precio = 10000
         print("El precio de la entrada es", "$", precio)
