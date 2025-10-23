24fba92829109f5904447924708c873e18197969
julianapreciado043-dotcom <julianapreciado043@gmail.com>
2025-10-23 14:15:46 -0500

    Comentario

--- Practica/Sena/Condicional 1.py
+++ Practica/Sena/Condicional 1.py
@@ -1,6 +1,6 @@
 key = "contraseña"
 password = input("Introduce la contraseña: ")
 if password == password.lower():
-    print("La ontraseña coincide")
+    print("La contraseña coincide")
 else:
     print("La contraseña no coincide")
